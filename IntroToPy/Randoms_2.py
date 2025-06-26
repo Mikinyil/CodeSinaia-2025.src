b30569bbafde8c961496bae12fa7316fafd081ca
Florin.Teo <Florin.Teo@live.com>
2025-06-26 09:22:55 -0700

    move to randoms_db.txt and add mountains_db.txt

--- IntroToPy/Randoms_2.py
+++ IntroToPy/Randoms_2.py
@@ -2,7 +2,7 @@ import ast
 
 ###
 # Loads a data set of random values from the disk
-# The file contains one random per line, each containing the random value followed by the zero-based iteration
+# The "randoms_db.txt" contains one random per line, each containing the random value followed by the zero-based iteration
 # where it was generated in the sequence. The method returns two values:
 # - the map associating each random value to the list of zero-based iterations where it was generated
 # - the total count of iterations
@@ -20,5 +20,5 @@ def load_randoms(randoms_file):
     return randoms_map, count
 
 if __name__ == "__main__":
-    randoms_map, count = load_randoms("dataset.txt")
+    randoms_map, count = load_randoms("IntroToPy/randoms_db.txt")
     print(f"Loaded {len(randoms_map.keys())} unique random generated in a sequence of {count} iterations")
