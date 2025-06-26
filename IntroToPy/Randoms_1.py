d7f1f0a3625f28cfc16c43637f218871dc2f57ac
Florin.Teo <Florin.Teo@live.com>
2025-06-25 21:52:01 -0700

    Polish Randoms_1.py

--- IntroToPy/Randoms_1.py
+++ IntroToPy/Randoms_1.py
@@ -1,11 +1,13 @@
 import random
+import statistics
 
-# read inputs parameters from the console
+# Read input parameters from the console
 count = 1000 # int(input("Number of values?> "))
 min_value = 12 # int(input("Minimum value?> "))
 max_value = 85 # int(input("Maximum value?> "))
+print(f"Generating {count} randoms in the range [{min_value}, {max_value}]")
 
-# generate count values in the range [min_value, max_value] and store them in a the values map
+# Generate count values in the range [min_value, max_value] and store them in a the values map
 # randoms_map - associates each random value to the zero-based index/iteration where it was generated
 # <key=random_value, value=[index1, index2, index3, ...]> 
 randoms_map = {}
@@ -15,32 +17,34 @@ for i in range(0, count):
         randoms_map[r] = []
     randoms_map[r].append(i)
 
-# write the random values in the order in which they were generated, in the file "dataset.txt"
+# Write a text file "dataset.txt" with each random on a line, its value followed by the indexes where it occurred
 with open("dataset.txt", "w") as data_file:
     for r in randoms_map.keys():
         data_file.write(f"{r} {randoms_map[r]}\n")
 
-# how many distinct randoms were generated?
+# Print a few statistics: min, max random values,  mean, median(aka average) and standard deviation (aka stdev) of the sequence.
 print(f"Count of unique randoms: {len(randoms_map.keys())}")
-
-# what's the smallest and the largest random?
 sorted_randoms = sorted(randoms_map.keys())
 print(f"Min random: {sorted_randoms[0]}; Max random: {sorted_randoms[-1]}")
+print(f"Mean of the sequence: {statistics.mean(sorted_randoms)}")
+print(f"Median of the sequence: {statistics.median(sorted_randoms)}")
+print(f"Standard Deviation of the sequence: {statistics.stdev(sorted_randoms)}")
 
-# what are the topN most frequent numbers?
+# Read from the input the desired topN count of most frequent randoms that should be printed.
+# Then, store in an array all the randoms along with their individual count. Each entry in the array is an object with two fields:
+# - value: the random value
+# - count: the value's frequency or count of occurrences in the sequence. 
+# In the end the array is sorted in the descending order of each object's _count_ value and the first topN are printed out.
 topN = 10 # int(input("Count of most frequent numbers?> "))
-# randoms_array - list of objects, each object containing the "value" and the "count" number of
-# times that value was generated
 randoms_array = []
 for random_value in randoms_map.keys():
     randoms_array.append({"value" : random_value, "count" : len(randoms_map[random_value])}) 
-# sort all objects in the array by their count field, in reverse order
 randoms_array.sort(key=lambda x: x["count"], reverse=True)
 
-# build a set of custom lines of text, each extracted from each object in the array
+# Use a lambda expresion to map the first _topN_ objects from the sorted array to a string showing a random value and the count of its occurrences in the sequence. 
+# Join these strings together, each a new line, then print the resulting text.
 lines = map(
     lambda r: f'random {r["value"]} occurred {r["count"]} times',
     randoms_array[:topN]
 )
-# join all the lines by the new-line character and print the result
 print("\n".join(lines))
