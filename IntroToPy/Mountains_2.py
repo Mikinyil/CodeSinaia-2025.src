6dfeda163342e8d5404ed8ae5f369136f2df6ba2
Florin.Teo <Florin.Teo@live.com>
2025-07-01 16:36:40 -0700

    Add Mountains_4

--- IntroToPy/Mountains_2.py
+++ IntroToPy/Mountains_2.py
@@ -24,6 +24,28 @@ def load_mountains(mountains_file):
             count += 1
     return mountains_map, count
 
+###
+# Loads a data set of mountains from the disk.
+# The method returns two values:
+# - a map associating each country ISO to the list of {mountain, elevation} pairs from within
+# - a map associating each country ISO to the country's full name. 
+def load_mountains2(mountains_file):
+    mountains_map = {}
+    countries_map = {}
+    with open(mountains_file, "r", encoding="utf-8-sig") as data_file:
+        for line in data_file.readlines():
+            line_parts = line.strip().split("\t")
+            mountain_name = line_parts[0]
+            mountain_elevation = ast.literal_eval(line_parts[1]) if line_parts[1] != "NULL" else None
+            country_name = line_parts[2]
+            country_iso = line_parts[3]
+            if not country_iso in mountains_map:
+                mountains_map[country_iso] = []
+                countries_map[country_iso] = country_name
+            mountains_map[country_iso].append({"name":mountain_name, "elevation":mountain_elevation})
+    return mountains_map, countries_map
+
+
 if __name__ == "__main__":
     mountains_map, count = load_mountains("IntroToPy/mountains_db.tsv")
     print(f"Loaded {count} mountains from {len(mountains_map.keys())} countries.")
