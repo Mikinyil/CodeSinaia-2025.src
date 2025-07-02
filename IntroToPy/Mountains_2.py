7e1d211ac17bc6607c7b93ae74cbd95912a10e63
Florin.Teo <Florin.Teo@live.com>
2025-07-01 22:28:02 -0700

    Add Mountains_5.ipynb

--- IntroToPy/Mountains_2.py
+++ IntroToPy/Mountains_2.py
@@ -1,7 +1,7 @@
 import ast
 
 ###
-# Loads a data set of mountains from the disk
+# Loads a dataset of mountains from the disk
 # The "mountains_db.tsv" contains one mountain per line, each line containing
 # several fields separated by TAB: mountain name, elevantion, country where it is located and
 # the ISO3 code of that country.
@@ -25,7 +25,7 @@ def load_mountains(mountains_file):
     return mountains_map, count
 
 ###
-# Loads a data set of mountains from the disk.
+# Loads a dataset of mountains from the disk.
 # The method returns two values:
 # - a map associating each country ISO to the list of {mountain, elevation} pairs from within
 # - a map associating each country ISO to the country's full name. 
@@ -44,6 +44,33 @@ def load_mountains2(mountains_file):
                 countries_map[country_iso] = country_name
             mountains_map[country_iso].append({"name":mountain_name, "elevation":mountain_elevation})
     return mountains_map, countries_map
+
+###
+# Loads a dataset of mountains and a dataset of countries into two sets in memory. The sets contain
+# objects, one for each row in the file, with the fields corresponding to the columns in the files.
+# The method returns the two sets.
+# - a set listing all mountains [{name, elevation, country, country_iso}]
+# - a set listing all countries [[country_iso, continent]]
+def load_mountains3(mountains_file, countries_file):
+    mountains_set = []
+    with open(mountains_file, "r", encoding="utf-8-sig") as data_file:
+        for line in data_file.readlines():
+            line_parts = line.strip().split("\t")
+            mountains_set.append({
+                "name": line_parts[0],
+                "elevation": ast.literal_eval(line_parts[1]) if line_parts[1] != "NULL" else None,
+                "country" : line_parts[2],
+                "country_iso" : line_parts[3]
+            })
+    countries_set = []
+    with open(countries_file, "r", encoding="utf-8-sig") as data_file:
+        for line in data_file.readlines():
+            line_parts = line.strip().split("\t")
+            countries_set.append({
+                "country_iso": line_parts[0],
+                "continent" : line_parts[1]
+            })
+    return mountains_set, countries_set
 
 
 if __name__ == "__main__":
