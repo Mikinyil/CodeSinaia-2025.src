6aee79d9e12ddcf1d3f1516203f3c6d958a39f03
Florin.Teo <Florin.Teo@live.com>
2025-07-08 21:19:19 -0700

    Adding Hello Ollama

---
+++ HelloOllama.py
@@ -0,0 +1,16 @@
+import ollama
+
+# Specify the model name
+model_name = "gemma3:1b"
+
+#print(dir(ollama))
+
+# Define the prompt
+prompt = "What is the capital of France?"
+
+# Send the prompt to the model and get the response
+response = ollama.generate(model=model_name, prompt=prompt)
+
+# Print the response
+print(type(response))
+print(response.response)
