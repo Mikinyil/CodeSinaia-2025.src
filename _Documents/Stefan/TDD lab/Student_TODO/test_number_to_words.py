c1cbbe55e1a74da72f2adff13355c793254496a2
Stefan Ene <enes@oregonstate.edu>
2025-07-15 01:32:53 -0700

    refactored TDD content and added TODO code

---
+++ _Documents/Stefan/TDD lab/Student_TODO/test_number_to_words.py
@@ -0,0 +1,11 @@
+import unittest
+from number_to_words import number_to_words
+
+class TestNums(unittest.TestCase):
+    
+    # TODO
+    # Adauga codul pentru unit tests aici
+
+    # sample test:
+    def sample_unit_test(self):
+        number_to_words(10)
